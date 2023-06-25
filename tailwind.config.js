01b17e2888b1117772d4afceb6708bce89778c5c
truong878664 <trandao056@gmail.com>
2023-06-25 22:45:42 +0700

    reponsive destop and mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        left: {
+          '0%': { left: '0' },
+          '100%': { left: '-100%' },
+        }
+      }
+    },
     colors: {
       main: "#F53838",
       white: "#fff",
@@ -14,6 +21,7 @@ module.exports = {
       custom:
         "3.9695122241973877px 7.371951103210449px 19.847558975219727px rgba(0, 0, 0, 0.09)",
     },
+    
   },
   plugins: [],
 };
